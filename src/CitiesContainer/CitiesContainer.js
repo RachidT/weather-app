61227056a1a1c9dadec4d251cd7be62063c909e3
Rachid <rachid@adomik.com>
2019-12-29 13:05:50 +0100

    set components architecture

---
+++ src/CitiesContainer/CitiesContainer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import CitiesWeather from 'CityWeather'
+
+const CitiesContainer = () => {
+  return (
+    <div>
+      <CitiesWeather />
+    </div>
+  );
+}
+
+export default CitiesContainer
---
+++ src/CitiesContainer/CitiesWeather/CitiesWeather.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import CurrentCityWeahter from './CurrentCityWeather'
+import ForcastCityWeather from './ForcastCityWeather'
+
+
+const CitiesWeather = () => {
+  return (
+    <div>
+      <CurrentCityWeahter />
+      <ForcastCityWeather />
+    </div>
+  )
+}
+
+export default CitiesWeather
